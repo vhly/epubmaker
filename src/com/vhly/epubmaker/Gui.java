53c4980bd3beef53e0afcec9bc7d6a87b9b9e741
Zhang Xiaohe <vhly@163.com>
2011-09-02 00:09:53 +0800

    添加代码

---
+++ src/com/vhly/epubmaker/Gui.java
@@ -0,0 +1,11 @@
+package com.vhly.epubmaker;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vhly[FR]
+ * EMail: vhly@163.com
+ * Date: 2011-9-1
+ * Time: 23:49:06
+ */
+public class Gui {
+}
---
+++ src/com/vhly/epubmaker/Main.java
@@ -0,0 +1,13 @@
+package com.vhly.epubmaker;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vhly
+ * Date: 2011-9-1
+ * Time: 23:47:04
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
