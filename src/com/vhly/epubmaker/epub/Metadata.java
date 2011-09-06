3a82de6d5359f6ee2947bea708493e8946287145
Zhang Xiaohe <vhly@163.com>
2011-09-06 13:49:41 +0800

    Add EPub file model

---
+++ src/com/vhly/epubmaker/epub/Metadata.java
@@ -0,0 +1,24 @@
+package com.vhly.epubmaker.epub;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 11-9-6
+ * Email: vhly@163.com
+ */
+
+/**
+ * Metadata for opf file
+ */
+public class Metadata {
+    private String identifier_BookID;
+    private String title;
+    private String rights;
+    private String publisher;
+    private String subject;
+    private String date;
+    private String description;
+    private String creator;
+    private String language;
+    private String cover_image;
+}
