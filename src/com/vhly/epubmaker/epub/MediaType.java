2a1b4de4fa5ba58b5b7301a3cc5a8eb680754234
Zhang Xiaohe <vhly@163.com>
2011-09-15 14:54:46 +0800

    Update README

---
+++ src/com/vhly/epubmaker/epub/MediaType.java
@@ -0,0 +1,25 @@
+package com.vhly.epubmaker.epub;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 11-9-14
+ * Email: vhly@163.com
+ */
+public enum MediaType {
+    PACKAGE_XML("application/oebps-package+xml"),
+    NCX("application/x-dtbncx+xml"),
+    CSS("text/css"),
+    XHTML("application/xhtml+xml"),
+    FONT("font/opentype"),
+    JPEG("image/jpeg"),
+    GIF("image/gif"),
+    PNG("image/png"),
+    ;
+
+    private String type;
+
+    MediaType(String s) {
+       type = s;
+    }
+}
---
+++ src/com/vhly/epubmaker/epub/content/Content.java
@@ -0,0 +1,11 @@
+package com.vhly.epubmaker.epub.content;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 11-9-15
+ * Email: vhly@163.com
+ */
+public interface Content {
+
+}
