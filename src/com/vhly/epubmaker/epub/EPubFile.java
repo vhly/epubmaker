8a656889a24679d35a8d09f4281ce4b776117c6a
Zhang Xiaohe <vhly@163.com>
2011-10-06 07:41:51 +0800

    Fix:     Fix EPubFile load() method allways return false;     Fix Chapter List cant scroll;
    (cherry picked from commit c58a35aab658554b0926fec5616e48bb3f5700a6)

--- src/com/vhly/epubmaker/epub/EPubFile.java
+++ src/com/vhly/epubmaker/epub/EPubFile.java
@@ -193,6 +193,7 @@ public class EPubFile extends ZipFile {
                                     }
                                 }
                             }
+                            bret = true;
                         }
                     }
                 }
