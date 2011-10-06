7b492740250ff4b55fd5537a77b11eba1b621931
Zhang Xiaohe <vhly@163.com>
2011-10-06 17:36:49 +0800

    Update Chapter Content show. (cherry picked from commit 7c7f8acbf2857918d18ed32f2b0c65a478a4e4ae)
    Conflicts:
    
    	src/com/vhly/epubmaker/epub/content/Chapter.java
    	src/com/vhly/epubmaker/reader/ChapterTreeModel.java
    	src/com/vhly/epubmaker/reader/EPubContentHandler.java
    	src/com/vhly/epubmaker/reader/EPubReader.java
    	src/com/vhly/epubmaker/reader/FileLoadThread.java

--- src/com/vhly/epubmaker/epub/content/Chapter.java
+++ src/com/vhly/epubmaker/epub/content/Chapter.java
@@ -2,13 +2,15 @@ package com.vhly.epubmaker.epub.content;
 
 import com.vhly.epubmaker.epub.MediaType;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by IntelliJ IDEA.
  * User: vhly[FR]
  * Date: 11-9-18
  * Email: vhly@163.com
  */
-public class Chapter implements Content{
+public class Chapter implements Content {
     /**
      * Chapter's title
      */
@@ -21,6 +23,8 @@ public class Chapter implements Content{
      * Chapter's mediatype, default is xhtml
      */
     private MediaType mediaType = MediaType.XHTML;
+
+    private String pageContent;
 
     // Content implements.
 
@@ -49,8 +53,36 @@ public class Chapter implements Content{
      * @param data byte[] data
      */
     public void setContent(byte[] data) {
-        if(data != null){
+        if (data != null) {
             content = data;
         }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPageContent() {
+        if (pageContent == null && content != null) {
+            try {
+                pageContent = new String(content,"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                pageContent = new String(content);
+            }
+        }
+        return pageContent;
+    }
+
+    public void setPageContent(String pageContent) {
+        this.pageContent = pageContent;
+    }
+
+    @Override
+    public String toString() {
+        return title != null ? title : "Chapter untitled";
     }
 }
